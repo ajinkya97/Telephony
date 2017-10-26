9ecd1e57eedc9ef64c6dd938f64abfe73d0f803b
ajinkya97 <ajinkyalonkar88.al@gmail.com>
2017-10-25 19:04:46 -0700

    Initial commit

---
+++ app/src/main/java/com/example/aj/telephony/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.aj.telephony;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView txt=(TextView)findViewById(R.id.txt);
+
+        TelephonyManager tm=(TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+
+        String IMEINumber=tm.getDeviceId();
+        String SIMSerialNumber=tm.getSimSerialNumber();
+        String networkCountryISO=tm.getNetworkCountryIso();
+        String SIMCountryISO=tm.getSimCountryIso();
+        String softwareVersion=tm.getDeviceSoftwareVersion();
+        String voiceMailNumber=tm.getVoiceMailNumber();
+
+        String strphoneType="";
+
+        int phoneType=tm.getPhoneType();
+
+        switch (phoneType)
+        {
+            case (TelephonyManager.PHONE_TYPE_CDMA):
+                strphoneType="CDMA";
+                break;
+            case (TelephonyManager.PHONE_TYPE_GSM):
+                strphoneType="GSM";
+                break;
+            case (TelephonyManager.PHONE_TYPE_NONE):
+                strphoneType="NONE";
+                break;
+        }
+
+        boolean isRoaming=tm.isNetworkRoaming();
+
+        String info="Phone Details:\n";
+        info+="\n IMEI Number:"+IMEINumber;
+        info+="\n Sim Serial Number:"+SIMSerialNumber;
+        info+="\n Network Country ISO:"+networkCountryISO;
+        info+="\n SIM Country ISO:"+SIMCountryISO;
+        info+="\n Software Version:"+softwareVersion;
+        info+="\n Voice Mail Number:"+voiceMailNumber;
+        info+="\n Phone Network Type:"+strphoneType;
+        info+="\n In Roaming? :"+isRoaming;
+
+        txt.setText(info);
+    }
+}
